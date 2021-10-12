5ea9e94c348e1d35010229c4ebb0053f4398f97c
Leo Fu <leo.fu@validic.com>
2021-10-11 22:15:08 -0400

    Updated comment

--- system_info.py
+++ system_info.py
@@ -3,6 +3,7 @@ import platform
 def kenerl_info():
 	'''
 	Return Linux kernel information
+	Return Linux kernel information
 	'''
 	kernel_info = platform.name()
 	return {'release': kernel_info.release, 'version': kernel_info.version}
